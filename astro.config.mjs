beebbd1401832bbdb40b952c1f697669f861e0b4
YFY <>
2024-12-21 10:22:04 +0800

    第三方框架和 script 的水合

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://musical-donut-d00741.netlify.app"
+  site: "https://musical-donut-d00741.netlify.app",
+  integrations: [preact()]
 });
